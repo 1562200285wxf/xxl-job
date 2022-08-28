21c9a8980ba740ce9f3370b98880087bac01bdce
wangxiaofeng <1562200285@qq.com>
2022-08-28 15:11:19 +0800

    扑克游戏

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/study/test.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/study/test.java
@@ -1,37 +1,77 @@
-import re
-n = int(input())
-        sentence = []
-        while 1:
-        s = input()
-        if s != "":
-        sentence.append(s)
-        else:
-        break
-        def is_num_leq_letter(pwd):
-        num  = len(re.findall(r"\d",pwd))
-        w = len(re.findall(r"[a-zA-Z]",pwd))
-        if num<w:
-        return True
-        else:
-        return False
-        def five_continue(pwd):
-        num  = len(re.findall(r"[a-zA-Z]{5,}",pwd))
-        if num>0:
-        return True
-        else:
-        return False
+package com.xxl.job.executor.study;
 
-        def isContinuousChar(pwd):
-        num  = len(re.findall(r"([a-zA-Z]|[0-9])\1",pwd))
-        if num>0:
-        return True
-        else:
-        return False
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
-        for s in sentence:
-        if len(s)>=6 and is_num_leq_letter(s) and not five_continue(s) and not isContinuousChar(s):
-        print('YES')
-        else:
-        print('NO')
-        print()
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2022-08-28 14:42
+ * @description：
+ */
+public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
 
+
+    }
+    public static String ttt(String numbers){
+        String[] strs = numbers.split(" ");
+        if(judgeCopy(strs,0) || judgeCopy(strs,3)){
+            if(judgeCopy(strs,3)){
+                if(strs[0].charAt(0)>strs[3].charAt(0)){
+                    return "Alice";
+                }else return "Bob";
+            }else return "Bob";
+        }else {
+            if(!judgeCopy2(strs,0).equals("0") || !judgeCopy2(strs,3).equals("0")){
+                if(!judgeCopy2(strs,0).equals("0")){
+                    if(judgeCopy2(strs,0).charAt(0)>judgeCopy2(strs,3).charAt(0)){
+                        return "Alice";
+                    }else return "Bob";
+                }else return "Bob";
+            }else  if(max(strs,0)>max(strs,3)){
+                return "Alice";
+
+            }else return "Bob";
+        }
+    }
+
+    public static boolean judgeCopy(String[] strs,int index){
+        int middle = index++;
+        if(strs[index].equals(strs[middle]) &&
+                strs[middle].equals(strs[middle++])){
+            return true;
+        }else return false;
+    }
+
+    public static String judgeCopy2(String[] strs,int index){
+
+        Set<String> set = new HashSet<>();
+        int middle = index++;
+        set.add(strs[index]);
+        if(set.contains(strs[index++])){
+            return strs[index++]
+        }else {
+            set.add(strs[index++]);
+        }
+        if(set.contains(strs[middle++])){
+            return strs[middle++];
+        }else {
+           return "0";
+        }
+    }
+
+    public static int max(String[] strs,int index){
+        String max = strs[index];
+        if(max.charAt(0)<strs[index++].charAt(0)){
+            max = strs[index++];
+        }
+        int middle = index++;
+        if(max.charAt(0)<strs[middle++].charAt(0)){
+            max = strs[middle++];
+        }
+        return max.charAt(0);
+    }
+}
