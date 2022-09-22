6b09fa3e438bfd5a1aaac0972b091f188af6c2f2
wangxiaofeng <1562200285@qq.com>
2022-09-22 10:33:36 +0800

    单例模式在缓存中的应用

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/study/design/signleton/SignletonCacheModel.java
@@ -0,0 +1,18 @@
+package com.xxl.job.executor.study.design.signleton;
+
+import lombok.Data;
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2022-09-22 10:21
+ * @description：
+ */
+@Data
+public class SignletonCacheModel {
+
+    String key;
+
+    String value;
+
+
+}
