0e65bcab86e9a51312a4322e24660e8fa753b6fb
wangxiaofeng <1562200285@qq.com>
2022-11-17 17:30:04 +0800

    【offer】---链表反转

---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/study/offer2/ListNode.java
@@ -0,0 +1,26 @@
+package com.xxl.job.executor.study.offer2;
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2022-11-17 17:20
+ * @description：
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static ListNode getInstance(){
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode4 = new ListNode(4,listNode5);
+        ListNode listNode3 = new ListNode(3,listNode4);
+        ListNode listNode2 = new ListNode(2,listNode3);
+        ListNode listNode1 = new ListNode(1,listNode2);
+        return listNode1;
+    }
+
+
+
+}
---
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/study/offer2/offer12.java
@@ -0,0 +1,35 @@
+package com.xxl.job.executor.study.offer2;
+
+
+
+/**
+ * @author ：wang xiaofeng
+ * @date ：Created in 2022-11-16 18:12
+ * @description：删除链表中的节点
+ */
+public class offer12 {
+    public static void main(String[] args) {
+        ListNode listNode = ListNode.getInstance();
+        removeNode(listNode,2);
+        System.out.println(listNode);
+    }
+
+    public static ListNode removeNode(ListNode listNode,int target){
+        ListNode current = listNode;
+        if(listNode.val == target){
+            return listNode.next;
+        }
+        while (listNode.next != null && listNode.next.val == target){
+            if(listNode.next.next != null) {
+                listNode.next = listNode.next.next;
+            }else {
+                listNode.next = null;
+            }
+            listNode = listNode.next;
+        }
+        return current;
+    }
+
+
+
+}
